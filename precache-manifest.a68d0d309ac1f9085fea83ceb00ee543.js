3bbb01757c51988d7f7a059874880f0430b5009c
KimJunYoung1 <soxo1027@gmail.com>
2019-12-31 17:12:03 +0900

    Updates

---
+++ precache-manifest.a68d0d309ac1f9085fea83ceb00ee543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee77353d0fbcdb8061abe82c9b7f695",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "1fcbda7c83e5d512d4e5",
+    "url": "/movie_app_2019/static/css/main.82d3a71e.chunk.css"
+  },
+  {
+    "revision": "8526568d949b0ffebe66",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fcbda7c83e5d512d4e5",
+    "url": "/movie_app_2019/static/js/main.307f6b54.chunk.js"
+  },
+  {
+    "revision": "101d7acd549509bcf93b",
+    "url": "/movie_app_2019/static/js/runtime-main.69d003e7.js"
+  }
+]);
